7265ce1d387fa19b5ed6a616bbb63d0ed0f8aae4
Wolfram Kriesing <wk@uxebu.com>
2010-05-07 22:29:45 +0200

    Build works now, removing doubles left to do.

--- tools/build.js
+++ tools/build.js
@@ -1,3 +1,129 @@
+if (typeof console=="undefined"){
+	console = {
+		log:function(){
+			print([].join.call(arguments, " "));
+		}
+	}
+}
+
+var SRC_DIR = "../src/";
+
+//
+//	Parameters.
+//
+var target = arguments[0];
+// Split the modules that shall only be included, e.g. oo,array => ["oo", "array"]
+var features = arguments[1] ? arguments[1].split(",") : [];
+
+// We store some global information here, so we dont need to pass them around.
+var globals = {
+	modules:{},
+	dependencyData:{}, // Indexed by the directory where the __deps__.js file was loaded from, so we dont load them multiple times.
+	modulesAdded:{}
+};
+
+
+
+function _loadJsonFile(fileName, throwError){
+	var ret = null;
+	try{
+		eval("ret = "+readFile(fileName));
+	}catch(e){
+		if (typeof throwError=="undefined" || throwError!=false){
+			console.log("\n\n=== Error reading file '" + fileName + "' at line "+ e.lineNumber +" ===");
+			for (var key in e){ console.log(key, e[key]) }
+		}
+	}
+	return ret;
+}
+
+function main(){
+	// summary:
+	// 		Load the platform JSON file (like Android.json) which contains all the features mapped to the exact js files.
+	// description:
+	// 		If features are given resolve teh dependencies and concat the files resulting form that.
+	var modules = _loadJsonFile(target);
+	globals.modules = modules;
+	var files = [];
+	if (features.length==0){
+		for (var m in modules){
+			var moduleFiles = modules[m].map(resolveDeps)
+			console.log("Adding module:", m, moduleFiles.length ? ("- " + moduleFiles.join(", ")) : "");
+			files = files.concat(moduleFiles);
+			globals.modulesAdded[m] = true;
+		}
+	} else {
+		for (var i=0, l=features.length, f; i<l; i++){
+			var moduleFiles = modules[features[i]].map(resolveDeps);
+			console.log("Adding module:", features[i], moduleFiles.length ? ("- " + moduleFiles.join(", ")) : "");
+			files = files.concat(moduleFiles);
+		}
+	}
+	// Remove doubles but never the first occurence, since this would break the file order dependencies.
+	var files = files.map(function(item, index){ return (files.slice(0, index).indexOf(item)!=-1) ? null : item; })
+					.filter(function(item){ return item==null ? false : true });
+							
+	print("\n\n");
+	print(files);
+};
+
+function resolveFeature(feature){
+	var ret = [];
+	if (!globals.modulesAdded[feature]){
+		console.log('Resolving feature: ', feature);
+		var parts = feature.split("-");
+		var ns = parts[0]; // The namespace of the feature, like "oo" in "oo" or "oo-declare or "oo-extend".
+		var f = parts.length>1 ? parts[1] : null; // The exact feature if given, like for "oo-declare".
+		var data = globals.modules;
+		if (f===null){
+			// If the feature is only "oo" then we use ALL files given inside "oo".
+			ret = data[ns];
+			for (var k in ret){
+				resolveDeps(ret[k], function(files){
+					ret = ret.concat(files);
+				});
+			}
+		} else {
+			// A features like "oo-declare" means we only want the features inside "oo"
+			// where the file is "*declare.*", if given of course.
+			for (var j=0, l=data[ns].length, file; j<l; j++){
+				file = data[ns][j];
+				if (file.indexOf(f)!=-1){
+					ret.push(file);
+					resolveDeps(file, function(files){
+						ret = ret.concat(files);
+					});
+				}
+			}
+		}
+	} else {
+		console.log("  Depends on '" + feature + "', already resolved before.");
+	}
+	return ret;
+}
+
+
+function resolveDeps(file){
+	// summary:
+	// 		Resolve the dependencies of file (e.g. array/_default.js).
+	// description:
+	// 		If a __deps__.json file exists in the path where the js file is in, e.g. "array",
+	// 		then we load all according files again (which are namely features, which need to be
+	// 		mapped to files again, resolveFeature() does that).
+	if (typeof globals.dependencyData[file]=="undefined"){
+		var path = file.split("/");
+		var f = path.pop(); // The filename e.g. "_default.js"
+		var deps = _loadJsonFile(SRC_DIR + path + "/__deps__.json", false);
+		//globals.dependencyData[file] = (typeof deps[f]!="undefined" ? deps[f] : []).map(resolveFeature);
+		globals.dependencyData[file] = (deps && typeof deps[f]!="undefined" ? deps[f] : [])
+										.map(resolveFeature) // Resolve the features
+										.filter(function(i){return !!i;}); // Return empty elements that map might had returned.
+	}
+	return [file].concat(globals.dependencyData[file]); // I don't know if we want to KNOW here that we have to concat "file" too.
+};
+
+main();
+/*
 // The arguments to this file shall be the features that this build shall contain.
 // E.g. call like this:
 //		jsc build.js -- Android oo-declare,oo-mixin,fx
@@ -26,6 +152,7 @@ function _callback(data){
 	// Remove doubles but never the first occurence, since this would break the file order dependencies.
 	var allFiles = allFiles.map(function(item, index){ return (allFiles.slice(0, index).indexOf(item)!=-1) ? null : item; })
 							.filter(function(item){ return item==null ? false : true });
+
 	print(allFiles.join(" "));
 }
 
@@ -96,16 +223,11 @@ function _depsCallback(data){
 }
 
 
-if (typeof console=="undefined"){
-	console = {
-		log:function(){
-			print([].join.call(arguments, " "));
-		}
-	}
-}
 
 try{
 	load(target);
 }catch(e){
 	print(e);
 }
+*/
+
